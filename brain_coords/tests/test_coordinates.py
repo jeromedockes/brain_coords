c4edc302e3b068485f660744ee15c972b34d94de
Jerome Dockes <jerome@dockes.org>
2020-05-15 17:38:47 -0400

    add test

--- brain_coords/tests/test_coordinates.py
+++ brain_coords/tests/test_coordinates.py
@@ -1,14 +1,17 @@
-import hashlib
 import numpy as np
 from nilearn import image
 
 from brain_coords import coordinates
 
 
-def test_load_coords():
+def test_load_coords(tmp_path):
     coords = coordinates.load_coords()
     assert coords.shape == (4, 3)
     assert np.allclose(coords[0], [0, -52, 18])
+    new_file = tmp_path / "coords"
+    np.savetxt(str(new_file), coords)
+    new_coords = coordinates.load_coords(str(new_file))
+    assert np.allclose(coords, new_coords)
 
 
 def test_coords_to_img():
