ccf61ffebaf7c660308e81157c9ba21a8a9c7e8f
Jerome Dockes <jerome@dockes.org>
2020-05-16 17:41:08 -0400

    check array size and fwhm value

--- brain_coords/coordinates.py
+++ brain_coords/coordinates.py
@@ -12,6 +12,7 @@ def load_dmn_coords():
 
 
 def coords_to_img(coords, fwhm=9.0):
+    coords = np.atleast2d(coords)
     mask = load_mni152_brain_mask()
     masker = NiftiMasker(mask).fit()
     voxels = np.asarray(
@@ -21,6 +22,9 @@ def coords_to_img(coords, fwhm=9.0):
     peaks = np.zeros(mask.shape)
     np.add.at(peaks, tuple(voxels.T), 1.0)
     peaks_img = image.new_img_like(mask, peaks)
-    img = image.smooth_img(peaks_img, fwhm=fwhm)
+    if fwhm is not None and fwhm >= 0:
+        img = image.smooth_img(peaks_img, fwhm=fwhm)
+    else:
+        print("not smoothing image")
     img = masker.inverse_transform(masker.transform(img).squeeze())
     return img
